906f0e5114e08bec3ce03d83cb126b0e60f9a8d2
Pr0p1k <propiktunes@gmail.com>
2018-07-29 23:12:41 +0300

    Script creates an image with a dot.

--- Site/src/handler.php
+++ Site/src/handler.php
@@ -1,5 +1,16 @@
 <?php
+$R_PX = 120;    //R value in pixels
+$X_PX = 166;    //X coordinate of center
+$Y_PX = 166;    //Y coordinate of center
 $X = $_REQUEST['X'];
-echo $X;
-?>
-
+$Y = 2; //$_REQUEST['Y0'];
+$R = $_REQUEST['R'];
+$belongsToArea = false;
+if ($X > 0 & $Y > 0 & $X <= $R & $Y <= $R) $belongsToArea = true;
+elseif ($X > 0 & $Y < 0 & $X * $X + $Y * $Y < $R * $R) $belongsToArea = true;
+elseif ($X < 0 & $Y < 0 & $Y > -2 * $X - $R) $belongsToArea = true;
+$img = imagecreatefrompng("img/area_final.png");
+imagefilledellipse($img, $X / $R * $R_PX + $X_PX, $Y / $R * $R_PX + $Y_PX, 3,
+    3, $belongsToArea ? 255 * 256 : 255 * 65536);
+header('Content-type: image/png');
+ImagePng($img);
--- Site/src/script.js
+++ Site/src/script.js
@@ -6,15 +6,17 @@ function check() {
         alert("Value X is not correct");
     } else if (y == undefined) alert("Value Y is not chosen");
     else if (str == '') alert("Value X is not entered")
-    else document.getElementById("calculate").submit();
+    else {
+        document.getElementById("calculate").submit();
+    }
 }
 
 var y;
 var previous;
 
-function set(button) {
+function setY(button) {
     if (previous != undefined) previous.style.backgroundColor = window
-        .getComputedStyle(document.getElementsByClassName('button').item(0)).backgroundColor;
+        .getComputedStyle(document.getElementsByClassName('btn').item(0)).backgroundColor;
     button.style.backgroundColor = 'chartreuse';
     previous = button;
     y = button.value;
